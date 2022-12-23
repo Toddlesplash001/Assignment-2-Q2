20510e1416f414d79aa95b9e248cc23a90edeb50
Toddlesplash001 <118872908+Toddlesplash001@users.noreply.github.com>
2022-12-23 09:06:21 +0530

    Add files via upload

---
+++ Assignment 2 Q2.py
@@ -0,0 +1,8 @@
+NAME = str(input("Enter your name "))
+SID = int(input("Enter your SID "))
+DepartmentName = str(input("Enter your Department name "))
+CGPA = float(input("Enter your CGPA "))
+print("Hey,",(NAME), "here!")
+print("My SID is",(SID))
+print("I am from", (DepartmentName), "department and my CGPA is",(CGPA))
+
